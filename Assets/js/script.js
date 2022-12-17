2c99e54dd9c63b9935fcb131d0a214b29fbf8a73
Anjali Roland <anjaligroland@gmail.com>
2022-12-17 15:08:30 -0800

    added some variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,7 @@
 
-var APIKey = "e5997cecec74377c7114fa3367f39a12"
+var APIKey = "e5997cecec74377c7114fa3367f39a12";
+
+var currentSearch = document.querySelector("input");
+var searchBtn = document.querySelector("searchBtn");
+var clearBtn = document.querySelector("clearBtn");
+
